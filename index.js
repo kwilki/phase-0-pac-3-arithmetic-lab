58d70105759ee5b1d17513e3569395a97e356c09
Kyle <kylewilkinson14@gmail.com>
2021-10-12 17:48:11 +1100

    completed test

---
+++ index.js
@@ -0,0 +1,8 @@
+const add = (a,b) => {return a + b}
+const subtract = (a, b) => {return a - b}
+const multiply = (a, b) => {return a * b}
+const divide = (a, b) => {return a / b}
+const increment = (n) => {return ++n}
+const decrement = (n) => {return --n}
+const makeInt = (string) => {return parseInt(string, 10)}
+const preserveDecimal = (string) => {return parseFloat(string)}
